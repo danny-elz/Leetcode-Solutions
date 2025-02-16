631190590cdd1177fa402ecc9449f46aa66ce6a4
Danny <109995999+danny-elz@users.noreply.github.com>
2025-02-15 21:13:25 -0500

    commit

---
+++ problems/ContainerWithMostWater.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Danny Elzein on 2025-02-15.
+//
+#include <vector>
+
+int maxArea(std::vector<int>& height) {
+    int l = 0;
+    int r = height.size() - 1;
+    int res;
+
+    while(l < r) {
+        int area = (r-l) * std::min(height[l], height[r]);
+        res = std::max(res, area);
+        if(height[l] < height[r]) {
+            l++;
+        } else if (height[l] > height[r]) { // we could get rid of this block however I'll leave it for clarity
+            r--;
+        } else
+            r--;
+    }
+    return res;
+}
